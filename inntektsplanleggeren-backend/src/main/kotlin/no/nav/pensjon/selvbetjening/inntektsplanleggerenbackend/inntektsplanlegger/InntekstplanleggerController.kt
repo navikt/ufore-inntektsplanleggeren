f53fba7fc9e7a74982b47b99b045a27250e14118
Anders Bye <anders.bye@nav.no>
2024-09-10 10:41:13 +0200

    PEB-687 properties and som minor changes

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntektsplanlegger/InntekstplanleggerController.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntektsplanlegger/InntekstplanleggerController.kt
@@ -1,9 +1,9 @@
 package no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntektsplanlegger
 
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntektsplanlegger.dto.*
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.pensjon.PensjonService
@@ -19,24 +19,26 @@ class InntektsplanleggerController(
     private val log = LoggerFactory.getLogger(InntektsplanleggerController::class.java)
 
     @GetMapping("loepende-beregningsperioder")
-    fun getLopendeBeregningsperioder(): LopendeBeregningsPerioderResponse {
-        val dummyPid = "12345678901"  //FIXME
+    fun getLopendeBeregningsperioder(): ResponseEntity<Any> {
+        val dummyPid = "26853949913"  //FIXME
         try {
             log.info("Henter lopende beregningsperioder for inntektsplanleggeren")
             val perioder = pensjonService.getLopendeBeregningsperiode(dummyPid)
-            return LopendeBeregningsPerioderResponse(perioder["iAar"]!!, perioder["nesteAar"]!!)
+            val tmp = LopendeBeregningsPerioderResponse(perioder["iAar"]!!, perioder["nesteAar"]!!)
+            //val responseEntity = ResponseEntity<Any>(tmp, HttpStatus.OK)
+            return ResponseEntity(tmp, HttpStatus.OK)
         } catch (exception: Exception) {
             throw ErrorHandler.exceptionToErrorResponse(exception, dummyPid)
         }
     }
 
     @GetMapping("initiate")
-    fun getInntektsplanleggerenInitialData(): InntektsplanleggerenInitialResponse {
-        val dummyPid = "12345678901"  //FIXME
+    fun getInntektsplanleggerenInitialData(): ResponseEntity<Any>  {
+        val dummyPid = "26853949913"  //FIXME
         return try {
             log.info("Henter basic data for inntektsplanleggeren")
 
-            inntektsPlanleggerService.getInntektsplanleggerInitialResponse(dummyPid)
+            ResponseEntity(inntektsPlanleggerService.getInntektsplanleggerInitialResponse(dummyPid), HttpStatus.OK)
         } catch (exception: Exception) {
             throw ErrorHandler.exceptionToErrorResponse(exception, dummyPid)
         }
