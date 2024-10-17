60bc4f93fc6818a539c4f5d84e6d4a0c9c044421
Juliana Coelho Maccarini Skrøvseth <juliana.coelho.maccarini.skrovseth@nav.no>
2024-10-17 16:19:51 +0200

    build errors

--- inntektsplanleggeren-frontend/src/routes.tsx
+++ inntektsplanleggeren-frontend/src/routes.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext} from "react";
 import {Routes, Route, BrowserRouter, Outlet, Navigate} from "react-router-dom";
 import {InitialView} from "@/components/initialView/InitialView";
 import App from "@/App";
