852b7e014c19d5337a05f27fe90d73fcb2a5152d
Juliana Coelho Maccarini Skrøvseth <juliana.coelho.maccarini.skrovseth@nav.no>
2024-11-05 12:31:55 +0100

    form field validation change

--- inntektsplanleggeren-frontend/src/api/apiFetching.tsx
+++ inntektsplanleggeren-frontend/src/api/apiFetching.tsx
@@ -11,7 +11,7 @@ import {
 
 const basePath = "/pensjon/selvbetjening/inntektsplanleggeren";
 
-const MOCKS_ENABLED = true;
+const MOCKS_ENABLED = false;
 
 export async function getInitiate(): Promise<GetInntektsgrenseResponse> {
     const searchParams = new URLSearchParams(document.location.search)
