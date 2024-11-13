262f551c86c98bfc5e958f23ab54184266ba1f79
Anders Bye <anders.bye@nav.no>
2024-11-13 10:40:27 +0100

    PEB-863-Inntektsplanleggeren-Innsendingstidpunkt-skal-returneres-i-response-fra-/send-endepunkt

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntektsplanlegger/InntektsplanleggerenSendResponse.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntektsplanlegger/InntektsplanleggerenSendResponse.kt
@@ -1,8 +1,15 @@
 package no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntektsplanlegger
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonProperty
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntektsplanlegger.validation.InntektsplanleggerMessage
+import java.time.LocalDateTime
 
-data class InntektsplanleggerenSendResponse(val messages: List<InntektsplanleggerMessage>, val status: InnsendingStatus)
+data class InntektsplanleggerenSendResponse(val messages: List<InntektsplanleggerMessage>,
+                                            val status: InnsendingStatus,
+                                            @JsonProperty("innsendingsTidspunkt")
+                                            @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+                                            val innsendingsTidspunkt: LocalDateTime)
 
 enum class InnsendingStatus{
     AUTOMATISK_BEHANDLING,
