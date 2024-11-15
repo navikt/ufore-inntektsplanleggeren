242da9b1ea780d35d99966dfaa149fea3fb13835
Juliana Coelho Maccarini Skrøvseth <juliana.coelho.maccarini.skrovseth@nav.no>
2024-11-15 19:07:41 +0100

    change persistence url

--- inntektsplanleggeren-frontend/src/components/initialView/InitialView.tsx
+++ inntektsplanleggeren-frontend/src/components/initialView/InitialView.tsx
@@ -17,13 +17,13 @@ import {YearView} from "@/components/initialView/YearView";
 import "./InitialView.css"
 import {DataContext} from "@/DataContextProvider";
 import {FormStateContext} from "@/context/FormData";
-import {deleteState, getInntekter, getState, saveState} from "@/api/apiFetching";
+import {deleteState, getInntekter, getState} from "@/api/apiFetching";
 import {ExpectedIncomeBox} from "@/components/initialView/ExpectedIncomeBox";
 import {MessageCodes} from "@/api/model/MessageCodes";
 
 export function InitialView() {
     const {initialViewData, messages, setInntekterResponse} = useContext(DataContext)
-    const {selectedYear, setBrukerinntekt, setAnnenForelderInntekt, brukerinntekt, annenForelderInntekt} = useContext(FormStateContext)
+    const {selectedYear, setBrukerinntekt, setAnnenForelderInntekt} = useContext(FormStateContext)
     const [errorMessage, setErrorMessage] = useState<string | null>(null)
     const navigate = useNavigate()
     const [isLoading, setIsLoading] = useState<boolean>(false)
