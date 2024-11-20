4ab5d81395199d8077f04474bf69812d0b67e7a9
Sindre Rasmussen <sindre.rasmussen@nav.no>
2024-11-20 11:22:33 +0100

    Added masked epsPid to InntekterResponse.

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntektsplanlegger/inntekt/InntekterResponse.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntektsplanlegger/inntekt/InntekterResponse.kt
@@ -5,7 +5,8 @@ data class InntekterResponse(
     val arbeidsinntektOgYtelserHittilIAar: List<AccumulatedMaanedsinntekt>,
     val pensjonFraAndreHittilIAar: List<AccumulatedMaanedsinntekt>?,
     val forventedeInntekter: ForventedeInntekter,
-    val uforeHeleAaret: Boolean
+    val uforeHeleAaret: Boolean,
+    val epsPid: String?
 )
 
 data class AccumulatedMaanedsinntekt(val maned: Int, val belop: Double?, val inntektsgivere: List<String>)
