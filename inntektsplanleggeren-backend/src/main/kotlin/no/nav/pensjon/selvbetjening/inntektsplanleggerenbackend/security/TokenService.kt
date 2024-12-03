8299643b7bfbdaad570e43b1472fe576c7083b73
Juliana Coelho Maccarini Skrøvseth <juliana.coelho.maccarini.skrovseth@nav.no>
2024-12-03 13:24:23 +0100

    some weird merging problem

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/TokenService.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/TokenService.kt
@@ -132,9 +132,9 @@ class TokenService(
     private fun typeOf(jwt: Jwt, pid: String, appId: AppId): TokenType {
         val issuer = jwt.getClaim<String>("iss")
         if (issuer == azureAdIssuer) {
-            if ((jwt.getClaim<String>("roles") == null) && appId.useAzureOBo) {
+            if ((jwt.getClaim<String>("roles") == null)) {
                 return TokenType.AZURE_AD_ON_BEHALF_OF
-            } else if ((jwt.getClaim<String>("sub") == jwt.getClaim<String>("oid")) || !appId.useAzureOBo) {
+            } else if ((jwt.getClaim<String>("sub") == jwt.getClaim<String>("oid"))) {
                 return TokenType.AZURE_AD_CLIENT_CREDENTIALS
             }
         } else if (issuer == tokenXIssuer) {
