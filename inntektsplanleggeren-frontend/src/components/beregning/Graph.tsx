d982a6d9908d4518a5dcc0b85832dee3a6067c88
Sindre Rasmussen <sindre.rasmussen@nav.no>
2024-12-05 11:17:46 +0100

    PEB-913: Showing one column when before values are null, adding border to coulmns in graph.

--- inntektsplanleggeren-frontend/src/components/beregning/Graph.tsx
+++ inntektsplanleggeren-frontend/src/components/beregning/Graph.tsx
@@ -4,64 +4,84 @@ import HighchartsReact from 'highcharts-react-official';
 import {SimulationResult} from "@/api/model/ApiRequests";
 
 const NUMBER_FORMATTER = new Intl.NumberFormat('nb-NO', { style: 'decimal', useGrouping: true });
+const getXAxisCategories = (isBeforeValuesAvailable: boolean) => {
+    if (isBeforeValuesAvailable) {
+        return ['I dag', 'Med dine endringer']
+    } else {
+        return ['Med dine endringer']
+    }
+}
 
-const GRAPH_DATA: Options = {
-    chart: {
-        type: 'column',
-        marginTop: 40
-    },
-    title: undefined,
-    credits: undefined,
-    xAxis: {
-        categories: ['I dag', 'Med dine endringer'],
-        labels: {
-            style: {
-                fontWeight: 'bold'
-            }
+const GRAPH_DATA = (isBeforeValuesAvailable: boolean) => {
+    return {
+        chart: {
+            type: 'column',
+            marginTop: 40
         },
-        
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            align: 'high',
-            offset: 15,
-            text: 'Kroner',
-            rotation: 0,
-            y: -30,
-            style: {
-                fontSize: '15px',
-            }
+        title: undefined,
+        credits: undefined,
+        xAxis: {
+            categories: getXAxisCategories(isBeforeValuesAvailable),
+            labels: {
+                style: {
+                    fontWeight: 'bold'
+                }
+            },
+
         },
-        stackLabels: {
-            enabled: true
-        },
-        labels: {
-            formatter: ({value}) => NUMBER_FORMATTER.format(typeof value === "string" ? Number.parseInt(value, 10) : value),
-        }
-    },
-    plotOptions: {
-        column: {
-            stacking: 'normal',
-            dataLabels: {
+        yAxis: {
+            min: 0,
+            title: {
+                align: 'high',
+                offset: 15,
+                text: 'Kroner',
+                rotation: 0,
+                y: -30,
+                style: {
+                    fontSize: '15px',
+                }
+            },
+            stackLabels: {
                 enabled: true
             },
-        }
-    },
-    legend: {
-        symbolHeight: 15, //size of legend circle
-        itemStyle: {
-            color: '#010B18AD',
-            fontSize: '17px', // TODO: how to use stantdard nav font?
-            newLine: true
+            labels: {
+                formatter: ({value}) => NUMBER_FORMATTER.format(typeof value === "string" ? Number.parseInt(value, 10) : value),
+            }
         },
-        itemDistance: 40,
-        // itemWidth: 250,
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true
+                },
+            }
+        },
+        legend: {
+            symbolHeight: 15, //size of legend circle
+            itemStyle: {
+                color: '#010B18AD',
+                fontSize: '17px', // TODO: how to use stantdard nav font?
+                newLine: true
+            },
+            itemDistance: 40,
+            // itemWidth: 250,
         }
+    }
+};
 
-    };
+const COULMN_STYLE = (isBeforeValuesAvailable: boolean) => {
+    return {
+        maxPointWidth: isBeforeValuesAvailable ? undefined : 200,
+        borderWidth: 1,
+        borderColor: "#303030",
+        dataLabels: {
+            enabled: false
+        }
+    }
+}
 
 export const Graph = (props : { simulationResult : SimulationResult}) => {
+    const isBeforeValuesAvailable = props.simulationResult.sum.yearly.before !== null
     const tooltip: Options['tooltip'] = {
         headerFormat: '<b>{point.x}</b><br/>',
             pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
@@ -69,34 +89,28 @@ export const Graph = (props : { simulationResult : SimulationResult}) => {
 
     return (
         <VStack>
-            <HighchartsReact highcharts={Highcharts} options={{...GRAPH_DATA, tooltip, series: [{
+            <HighchartsReact highcharts={Highcharts} options={{...GRAPH_DATA(isBeforeValuesAvailable), tooltip, series: [{
+                ...COULMN_STYLE(isBeforeValuesAvailable),
                 name: props.simulationResult.gjenlevendetillegg ? 'Uføretrygd inkludert gjenlevendetillegg' : 'Uføretrygd',
-                dataLabels: {
-                enabled: false
-                },
                 data: [
-                    (props.simulationResult.uforetrygd.yearly.before ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.before ?? 0),
-                    (props.simulationResult.uforetrygd.yearly.after ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.after ?? 0)],
+                    isBeforeValuesAvailable ? ((props.simulationResult.uforetrygd.yearly.before ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.before ?? 0)) : undefined,
+                    (props.simulationResult.uforetrygd.yearly.after ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.after ?? 0)].filter(isNotUndefined),
                 color: "var(--a-deepblue-500)"
-            },
+                },
                 props.simulationResult.barnetilleggFellesbarn || props.simulationResult.barnetilleggSaerkullsbarn ? {
+                    ...COULMN_STYLE(isBeforeValuesAvailable),
                     name: 'Barnetillegg uføretrygd',
-                    dataLabels: {
-                        enabled: false
-                    },
                     data: [
-                        (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.before ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.before ?? 0),
-                        (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.after ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.after ?? 0)],
+                        isBeforeValuesAvailable ? ((props.simulationResult.barnetilleggSaerkullsbarn?.yearly.before ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.before ?? 0)): undefined,
+                        (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.after ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.after ?? 0)].filter(isNotUndefined),
                     color: "var(--a-purple-400)"
                     } : undefined,
             {
+                ...COULMN_STYLE(isBeforeValuesAvailable),
                 name: 'Din forventede inntekt',
-                dataLabels: {
-                enabled: false
-                },
                 data: [
-                    props.simulationResult.forventetInntekt.yearly.before ?? 0,
-                    props.simulationResult.forventetInntekt.yearly.after ?? 0],
+                    isBeforeValuesAvailable ? (props.simulationResult.forventetInntekt.yearly.before ?? 0) : undefined,
+                    props.simulationResult.forventetInntekt.yearly.after ?? 0].filter(isNotUndefined),
                 color: "var(--a-green-200)"
             }
             ].filter(isNotUndefined)}} />
