8a249b6ec9f33eaec47a5a95bc5fa7b58ad9451f
Sindre Rasmussen <sindre.rasmussen@nav.no>
2024-12-12 13:51:20 +0100

    PEB-864: Bugfix, failed formating date.

--- inntektsplanleggeren-frontend/src/common/Utils.ts
+++ inntektsplanleggeren-frontend/src/common/Utils.ts
@@ -10,7 +10,8 @@ export function belopSum(data: InntektDetaljer[]): number {
 }
 
 export function formatDateTime(value: Date): string {
-    return value.toLocaleDateString('no-NO', {day: 'numeric', month: 'long', year: 'numeric'}) + ' kl. ' + value.toLocaleTimeString('no-NO', {hour: '2-digit', minute:'2-digit'})
+    const date = new Date(value)
+    return date.toLocaleDateString('no-NO', {day: 'numeric', month: 'long', year: 'numeric'}) + ' kl. ' + date.toLocaleTimeString('no-NO', {hour: '2-digit', minute:'2-digit'})
 }
 
 export function formatDate(value: Date): string {
