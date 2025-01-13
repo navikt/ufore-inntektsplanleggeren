65f3c03193500d2646f200036588557c625957fc
Sindre Rasmussen <sindre.rasmussen@nav.no>
2025-01-13 16:26:04 +0100

    PEB-967: Added handling for SIMULERING_CONTAINS_MOTREGNING and FAKTOROMREGNET_ELLER_MANUELT_OVERSTYRT.

--- inntektsplanleggeren-frontend/src/components/beregning/BeregningPage.tsx
+++ inntektsplanleggeren-frontend/src/components/beregning/BeregningPage.tsx
@@ -28,6 +28,9 @@ export const BeregningPage = () => {
         navigate(getFullPathForPage(PageLinks.OPPSUMMERING));
     }
 
+    const showSimulering = !(simulationResponse?.messages.some(message => message.messageCode === MessageCodes.FAKTOROMREGNET_ELLER_MANUELT_OVERSTYRT
+        || message.messageCode === MessageCodes.SIMULERING_CONTAINS_MOTREGNING))
+
     if (simulationResponse?.result) return (
         <VStack gap="8">
             { simulationResponse.messages.some(message => message.messageCode === MessageCodes.USER_HAS_NO_LOPENDE_VEDTAK_YET) ?
@@ -75,28 +78,45 @@ export const BeregningPage = () => {
                 </Alert> : null
             }
 
-            <VStack gap="3">
-                <Heading size={"medium"}>Oversikt i graf</Heading>
-                <Graph simulationResult={simulationResponse?.result}/>
-            </VStack>
-
-            <VStack>
-                <Heading size={"medium"}>Oversikt i tabell</Heading>
-                <VStack gap="12">
-                    {simulationResponse?.result && <SimulationTable simulationResult={simulationResponse.result}></SimulationTable>}
-                    <VStack>
-                        <BodyLong><strong>Månedlig utbetaling av uføretrygd med dine endringer, før skatt: <FormatKroner value={simulationResponse?.result.sum.monthly.after ?? 0}/></strong></BodyLong>
-                        <ReadMore header="Månedsbeløp spesifisert">
-                            <VStack>
-                                <BodyLong>{ simulationResponse?.result?.gjenlevendetillegg ? "Uføretrygd inkludert gjenlevendetillegg: " : "Uføretrygd: "}
-                                    <FormatKroner value={(simulationResponse?.result?.uforetrygd.monthly.after ?? 0) + (simulationResponse?.result.gjenlevendetillegg?.monthly.after ?? 0)}/></BodyLong>
-                                { simulationResponse?.result.barnetilleggFellesbarn !== null ? <BodyLong>Barnetillegg for fellesbarn: <FormatKroner value={(simulationResponse?.result.barnetilleggFellesbarn.monthly.after ?? 0)}/></BodyLong> : null}
-                                { simulationResponse?.result.barnetilleggSaerkullsbarn !== null ? <BodyLong>Barnetillegg for særkullsbarn: <FormatKroner value={(simulationResponse?.result.barnetilleggSaerkullsbarn.monthly.after ?? 0)}/></BodyLong> : null}
-                            </VStack>
-                        </ReadMore>
+            {showSimulering ?
+                <>
+                    <VStack gap="3">
+                        <Heading size={"medium"}>Oversikt i graf</Heading>
+                        <Graph simulationResult={simulationResponse?.result}/>
                     </VStack>
-                </VStack>
-            </VStack>
+
+                    <VStack>
+                        <Heading size={"medium"}>Oversikt i tabell</Heading>
+                        <VStack gap="12">
+                            {simulationResponse?.result &&
+                                <SimulationTable simulationResult={simulationResponse.result}></SimulationTable>}
+                            <VStack>
+                                <BodyLong><strong>Månedlig utbetaling av uføretrygd med dine endringer, før
+                                    skatt: <FormatKroner
+                                        value={simulationResponse?.result.sum.monthly.after ?? 0}/></strong></BodyLong>
+                                <ReadMore header="Månedsbeløp spesifisert">
+                                    <VStack>
+                                        <BodyLong>{simulationResponse?.result?.gjenlevendetillegg ? "Uføretrygd inkludert gjenlevendetillegg: " : "Uføretrygd: "}
+                                            <FormatKroner
+                                                value={(simulationResponse?.result?.uforetrygd.monthly.after ?? 0) + (simulationResponse?.result.gjenlevendetillegg?.monthly.after ?? 0)}/></BodyLong>
+                                        {simulationResponse?.result.barnetilleggFellesbarn !== null ?
+                                            <BodyLong>Barnetillegg for fellesbarn: <FormatKroner
+                                                value={(simulationResponse?.result.barnetilleggFellesbarn.monthly.after ?? 0)}/></BodyLong> : null}
+                                        {simulationResponse?.result.barnetilleggSaerkullsbarn !== null ?
+                                            <BodyLong>Barnetillegg for særkullsbarn: <FormatKroner
+                                                value={(simulationResponse?.result.barnetilleggSaerkullsbarn.monthly.after ?? 0)}/></BodyLong> : null}
+                                    </VStack>
+                                </ReadMore>
+                            </VStack>
+                        </VStack>
+                    </VStack>
+                </>
+                :
+                <Alert variant="warning">
+                    Vi kan dessverre ikke vise hvordan den nye inntekten din vil påvirke utbetalingen din av uføretrygd.
+                    Du kan likevel sende inn din inntektsendring.
+                </Alert>
+            }
             
             <BodyLong><strong>Har du spørsmål? <Link href={PageLinks.KONTAKT} target="_blank">Kontakt oss (åpnes i ny fane)</Link></strong></BodyLong>
 
