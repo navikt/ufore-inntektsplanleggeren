3ab2836613672f48078f1158601a5010e0311ee1
Sindre Rasmussen <sindre.rasmussen@nav.no>
2025-01-13 15:41:59 +0100

    PEB-967: Added warning/info messages for OPPGITT_INNTEKT_OVER_INNTEKTSTAK, OPPGITT_INNTEKT_GIVES_LOWER_UFORETRYGD_THAN_ALREADY_UTBETALT and OPPGITT_INNTEKT_GIVES_MORE_UFORETRYGD_THAN_ALREADY_UTBETALT.

--- inntektsplanleggeren-frontend/src/components/beregning/BeregningPage.tsx
+++ inntektsplanleggeren-frontend/src/components/beregning/BeregningPage.tsx
@@ -53,6 +53,28 @@ export const BeregningPage = () => {
                     </VStack>
             </ReadMore>
 
+            { simulationResponse.messages.some(message => message.messageCode === MessageCodes.OPPGITT_INNTEKT_OVER_INNTEKTSTAK) ?
+                <Alert variant="warning">
+                    Inntekten du har lagt inn er høyere enn 80 prosent av inntekten du hadde før du ble ufør. Derfor får du ikke utbetalt uføretrygd resten av året. Har du fortsatt fått utbetalt for
+                    mye når året er slutt, kan du få et etteroppgjør hvor du må betale tilbake.
+                </Alert> : null
+            }
+
+            { simulationResponse.messages.some(message => message.messageCode === MessageCodes.OPPGITT_INNTEKT_GIVES_LOWER_UFORETRYGD_THAN_ALREADY_UTBETALT) ?
+                <Alert variant="warning">
+                    Inntekten du har lagt inn, er høyere enn inntekten vi har brukt til å beregne uføretrygden din. Din nye inntekt viser at du har fått utbetalt for mye uføretrygd. Derfor kan det være at du
+                    ikke får utbetalt uføretrygd resten av året. Får du barnetillegg, kan det hende at du ikke får det utbetalt resten av året. Har du fortsatt fått for mye utbetalt ved slutten av året,
+                    kan du få et etteroppgjør hvor du må betale tilbake.
+                </Alert> : null
+            }
+
+            { simulationResponse.messages.some(message => message.messageCode === MessageCodes.OPPGITT_INNTEKT_GIVES_MORE_UFORETRYGD_THAN_ALREADY_UTBETALT) ?
+                <Alert variant="info">
+                    Inntekten du nå har lagt inn, er lavere enn inntekten vi har brukt til å beregne uføretrygden din. Din nye inntekt viser at du har fått utbetalt for lite uføretrygd.
+                    Derfor vil du ikke få trekk i uføretrygden din resten av året. Har du fortsatt fått for lite utbetalt ved slutten av året, kan du få tilbake penger i etteroppgjøret.
+                </Alert> : null
+            }
+
             <VStack gap="3">
                 <Heading size={"medium"}>Oversikt i graf</Heading>
                 <Graph simulationResult={simulationResponse?.result}/>
