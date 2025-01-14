0113afa06faa089de9040626d3388facb0b20c63
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-01-14 21:15:35 +0100

    Log oboToken for debugging

--- inntektsplanleggeren-frontend/server/server.ts
+++ inntektsplanleggeren-frontend/server/server.ts
@@ -120,6 +120,8 @@ app.use(
       return res.sendStatus(401);
     }
 
+    logger.info("OBO Token test", oboToken);
+
     return createProxyMiddleware({
       target: `${env.inntektsplanleggerenBackendUrl}/api`,
       headers: {
