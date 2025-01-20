c445aef529ff471a3d1f14f840136074f07c2a79
Sindre Rasmussen <sindre.rasmussen@nav.no>
2025-01-20 14:08:23 +0100

    PEB-928: Added representasjon-banner in inntektsplanleggeren-frontend.

--- inntektsplanleggeren-frontend/src/main.tsx
+++ inntektsplanleggeren-frontend/src/main.tsx
@@ -6,7 +6,11 @@ import {AppRoutes} from "@/routes";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <DataContextProvider>
-          <AppRoutes />
+          <div className="microfrontend-container content-wrapper">
+              <representasjon-banner
+                  representasjonstyper="PENSJON_FULLSTENDIG,PENSJON_BEGRENSET,UFORETYGD_SKRIV,UFORETYGD_KOMMUNISER,UFORETYGD_LES,PENSJON_SUPERADMIN"></representasjon-banner>
+          </div>
+          <AppRoutes/>
       </DataContextProvider>
   </React.StrictMode>
 )
