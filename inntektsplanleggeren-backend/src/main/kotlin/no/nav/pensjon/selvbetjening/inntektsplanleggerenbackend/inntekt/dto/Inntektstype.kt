8fcfd71da3151ae1ec680db608d48828e66d2c1a
Sindre Rasmussen <sindre.rasmussen@nav.no>
2025-01-22 17:13:16 +0100

    PEB-864: Fixed wrong code name for eps naeringsinntekt.

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntekt/dto/Inntektstype.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntekt/dto/Inntektstype.kt
@@ -7,7 +7,7 @@ enum class Inntektstype(val code: String) {
     ANDRE_YTELSER_BRUKER("UFR_forv_andre_ytelser"),
     PENSJON_UTLAND_BRUKER("UFR_forv_pensjon_utland"),
     ARBEIDSINNTEKT_EPS("UFR_forv_arbeidsinnt_EPS"),
-    NAERINGSINNTEKT_EPS("UFR_forv_naeringsinntekt_EPS"),
+    NAERINGSINNTEKT_EPS("UFR_forv_naeringsinnt_EPS"),
     UTENLANDSINNTEKT_EPS("UFR_forv_utenlandsinnt_EPS"),
     ANDRE_YTELSER_EPS("UFR_forv_andre_ytelser_EPS"),
     PENSJON_UTLAND_EPS("UFR_forv_pensjon_utland_EPS"),
