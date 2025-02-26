d5bc35868dd26381e62a6d570fbf392b24122544
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-02-26 19:45:28 +0100

    Set backgrunnsfarge til hvit for graf labels

--- inntektsplanleggeren-frontend/src/components/beregning/Graph.tsx
+++ inntektsplanleggeren-frontend/src/components/beregning/Graph.tsx
@@ -1,189 +1,192 @@
-import {VStack} from "@navikt/ds-react";
-import Highcharts, {Options} from 'highcharts';
+import { VStack } from "@navikt/ds-react";
+import Highcharts, { Options } from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import {SimulationResult} from "@/api/model/ApiRequests";
-import {DESKTOP_WIDTH} from "@/FormContainer";
-import {useEffect, useState} from "react";
+import { SimulationResult } from "@/api/model/ApiRequests";
+import { DESKTOP_WIDTH } from "@/FormContainer";
+import { useEffect, useState } from "react";
 
 const NUMBER_FORMATTER = new Intl.NumberFormat('nb-NO', { style: 'decimal', useGrouping: true });
 
 const formatYAxisNumber = (isDesktop: boolean, value: string | number) => {
-    const valueAsString = value + ""
-    if(!isDesktop && valueAsString.length > 3) {
-        return NUMBER_FORMATTER.format(Number.parseInt(valueAsString.substring(0, valueAsString.length - 3), 10))
-    }
-    return NUMBER_FORMATTER.format(Number.parseInt(valueAsString))
+  const valueAsString = value + ""
+  if (!isDesktop && valueAsString.length > 3) {
+    return NUMBER_FORMATTER.format(Number.parseInt(valueAsString.substring(0, valueAsString.length - 3), 10))
+  }
+  return NUMBER_FORMATTER.format(Number.parseInt(valueAsString))
 }
 
 const getXAxisCategories = (isBeforeValuesAvailable: boolean) => {
-    if (isBeforeValuesAvailable) {
-        return ['I dag', 'Med dine endringer']
-    } else {
-        return ['Med dine endringer']
-    }
+  if (isBeforeValuesAvailable) {
+    return ['I dag', 'Med dine endringer']
+  } else {
+    return ['Med dine endringer']
+  }
 }
 
-const numberOfItems = (simulationResult : SimulationResult) => {
-    return (simulationResult.barnetilleggFellesbarn || simulationResult.barnetilleggSaerkullsbarn) ? 3 : 2;
+const numberOfItems = (simulationResult: SimulationResult) => {
+  return (simulationResult.barnetilleggFellesbarn || simulationResult.barnetilleggSaerkullsbarn) ? 3 : 2;
 }
 
 const GRAPH_DATA = (isBeforeValuesAvailable: boolean, isDesktop: boolean, numberOfItems: number) => {
-    return {
-        chart: {
-            type: 'column',
-            marginTop: 50,
-            height: 400 + (numberOfItems * 37),
-        },
-        title: undefined,
-        credits: undefined,
-        xAxis: {
-            categories: getXAxisCategories(isBeforeValuesAvailable),
-            labels: {
-                style: {
-                    fontSize: isDesktop ? 18 : 12,
-                    fontWeight: 600,
-                    fontFamily: 'var(--a-font-family)',
-                    color: 'var(--a-grayalpha-700)',
-                }
-            },
-
-        },
-        yAxis: {
-            min: 0,
-            maxPadding: 0.1,
-            title: {
-                align: 'high',
-                offset: isDesktop ? 13 : -50,
-                text: isDesktop ? 'Kroner' : 'Tusen kroner',
-                rotation: 0,
-                y: -30,
-                style: {
-                    fontSize: '16px',
-                    fontFamily: 'var(--a-font-family)',
-                    color: 'var(--a-grayalpha-700)'
-                }
-            },
-            stackLabels: {
-                enabled: true,
-                formatter(this:Highcharts.StackItemObject){
-                    return "Sum " + NUMBER_FORMATTER.format(this.total) +" kr"
-                },
-                style: {
-                    fontSize: isDesktop ? 18 : 12,
-                    color: 'var(--a-grayalpha-700)',
-                    textOutline: false,
-                    fontWeight: 600,
-                    fontFamily: 'var(--a-font-family)',
-                    align: 'center'
-                }
-            },
-            labels: {
-                formatter: ({value} : {value:string | number}) => formatYAxisNumber(isDesktop, value),
-                style:{
-                    fontSize: 16,
-                    fontFamily: 'var(--a-font-family)',
-                    color: 'var(--a-grayalpha-700)'
-                }
-            }
-        },
-        plotOptions: {
-            column: {
-                stacking: 'normal',
-                dataLabels: {
-                    enabled: true,
-                    padding: 10
-                }
-            }
-        },
-        legend: {
-            events: {
-                itemClick: function () {
-                    return false;
-                }
-            },
-            align: 'left',
-            x: 0,
-            enableMouseTracking: false,
-            symbolHeight: 15, //size of legend circle
-            itemHoverStyle: {
-                color: '#010B18AD'
-            },
-            itemStyle: {
-                color: '#010B18AD',
-                fontSize: '18px',
-                newLine: true,
-                cursor: 'auto'
-            },
-            itemDistance: 80,
-            itemMarginBottom: 15
-            // itemWidth: 250,
+  return {
+    chart: {
+      type: 'column',
+      marginTop: 50,
+      height: 400 + (numberOfItems * 37),
+    },
+    title: undefined,
+    credits: undefined,
+    xAxis: {
+      categories: getXAxisCategories(isBeforeValuesAvailable),
+      labels: {
+        style: {
+          fontSize: isDesktop ? 18 : 12,
+          fontWeight: 600,
+          fontFamily: 'var(--a-font-family)',
+          color: 'var(--a-grayalpha-700)',
         }
+      },
+
+    },
+    yAxis: {
+      min: 0,
+      maxPadding: 0.1,
+      title: {
+        align: 'high',
+        offset: isDesktop ? 13 : -50,
+        text: isDesktop ? 'Kroner' : 'Tusen kroner',
+        rotation: 0,
+        y: -30,
+        style: {
+          fontSize: '16px',
+          fontFamily: 'var(--a-font-family)',
+          color: 'var(--a-grayalpha-700)'
+        }
+      },
+      stackLabels: {
+        enabled: true,
+        formatter(this: Highcharts.StackItemObject) {
+          return "Sum " + NUMBER_FORMATTER.format(this.total) + " kr"
+        },
+        backgroundColor: 'white',
+        style: {
+          fontSize: isDesktop ? 18 : 12,
+          color: 'var(--a-grayalpha-700)',
+          textOutline: false,
+          fontWeight: 600,
+          fontFamily: 'var(--a-font-family)',
+          align: 'center',
+        }
+      },
+      labels: {
+        formatter: ({ value }: { value: string | number }) => formatYAxisNumber(isDesktop, value),
+        style: {
+          fontSize: 16,
+          fontFamily: 'var(--a-font-family)',
+          color: 'var(--a-grayalpha-700)',
+        }
+      }
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+          padding: 10
+        }
+      }
+    },
+    legend: {
+      events: {
+        itemClick: function () {
+          return false;
+        }
+      },
+      align: 'left',
+      x: 0,
+      enableMouseTracking: false,
+      symbolHeight: 15, //size of legend circle
+      itemHoverStyle: {
+        color: '#010B18AD'
+      },
+      itemStyle: {
+        color: '#010B18AD',
+        fontSize: '18px',
+        newLine: true,
+        cursor: 'auto'
+      },
+      itemDistance: 80,
+      itemMarginBottom: 15
+      // itemWidth: 250,
     }
+  }
 };
 
 const COLUMN_STYLE = (isBeforeValuesAvailable: boolean) => {
-    return {
-        states: {
-            hover: {
-                enabled: false
-            },
-            inactive: {
-                opacity: 1,
-            }
-        },
-        maxPointWidth: isBeforeValuesAvailable ? undefined : 200,
-        minPointLength: undefined,
-        dataLabels: {
-            enabled: false
-        }
+  return {
+    states: {
+      hover: {
+        enabled: false
+      },
+      inactive: {
+        opacity: 1,
+      }
+    },
+    maxPointWidth: isBeforeValuesAvailable ? undefined : 200,
+    minPointLength: undefined,
+    dataLabels: {
+      enabled: false
     }
+  }
 }
 
-export const Graph = (props : { simulationResult : SimulationResult}) => {
-    const [width, setWidth] = useState<number>(window.innerWidth);
-    const handleWindowSize = () => setWidth(window.innerWidth);
-    useEffect(() => {
-        window.addEventListener('resize', handleWindowSize);
-        return () => window.removeEventListener('resize', handleWindowSize);
-    });
-    const isDesktop = width > DESKTOP_WIDTH;
+export const Graph = (props: { simulationResult: SimulationResult }) => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const handleWindowSize = () => setWidth(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSize);
+    return () => window.removeEventListener('resize', handleWindowSize);
+  });
+  const isDesktop = width > DESKTOP_WIDTH;
 
-    const isBeforeValuesAvailable = props.simulationResult.sum.yearly.before !== null
-    const tooltip: Options['tooltip'] = {
-        enabled: false,
-        headerFormat: '<b>{point.x}</b><br/>',
-            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-    };
+  const isBeforeValuesAvailable = props.simulationResult.sum.yearly.before !== null
+  const tooltip: Options['tooltip'] = {
+    enabled: false,
+    headerFormat: '<b>{point.x}</b><br/>',
+    pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+  };
 
-    return (
-        <VStack>
-            <HighchartsReact highcharts={Highcharts} options={{...GRAPH_DATA(isBeforeValuesAvailable, isDesktop, numberOfItems(props.simulationResult)), tooltip, series: [{
-                ...COLUMN_STYLE(isBeforeValuesAvailable),
-                    name: props.simulationResult.gjenlevendetillegg ? 'Uføretrygd inkludert gjenlevendetillegg' : 'Uføretrygd',
-                    data: [
-                    isBeforeValuesAvailable ? ((props.simulationResult.uforetrygd.yearly.before ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.before ?? 0)) : undefined,
-                    (props.simulationResult.uforetrygd.yearly.after ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.after ?? 0)].filter(isNotUndefined),
-                color: 'var(--a-deepblue-500)',
-                },
-                props.simulationResult.barnetilleggFellesbarn || props.simulationResult.barnetilleggSaerkullsbarn ? {
-                    ...COLUMN_STYLE(isBeforeValuesAvailable),
-                    name: 'Barnetillegg uføretrygd',
-                    data: [
-                        isBeforeValuesAvailable ? ((props.simulationResult.barnetilleggSaerkullsbarn?.yearly.before ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.before ?? 0)): undefined,
-                        (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.after ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.after ?? 0)].filter(isNotUndefined),
-                    color: 'var(--a-purple-400)'
-                    } : undefined,
-            {
-                ...COLUMN_STYLE(isBeforeValuesAvailable),
-                name: 'Din forventede inntekt',
-                data: [
-                    isBeforeValuesAvailable ? (props.simulationResult.forventetInntekt.yearly.before ?? 0) : undefined,
-                    props.simulationResult.forventetInntekt.yearly.after ?? 0].filter(isNotUndefined),
-                color: 'var(--a-green-400)'
-            }
-            ].filter(isNotUndefined)}} />
-        </VStack>
-    )
+  return (
+    <VStack>
+      <HighchartsReact highcharts={Highcharts} options={{
+        ...GRAPH_DATA(isBeforeValuesAvailable, isDesktop, numberOfItems(props.simulationResult)), tooltip, series: [{
+          ...COLUMN_STYLE(isBeforeValuesAvailable),
+          name: props.simulationResult.gjenlevendetillegg ? 'Uføretrygd inkludert gjenlevendetillegg' : 'Uføretrygd',
+          data: [
+            isBeforeValuesAvailable ? ((props.simulationResult.uforetrygd.yearly.before ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.before ?? 0)) : undefined,
+            (props.simulationResult.uforetrygd.yearly.after ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.after ?? 0)].filter(isNotUndefined),
+          color: 'var(--a-deepblue-500)',
+        },
+        props.simulationResult.barnetilleggFellesbarn || props.simulationResult.barnetilleggSaerkullsbarn ? {
+          ...COLUMN_STYLE(isBeforeValuesAvailable),
+          name: 'Barnetillegg uføretrygd',
+          data: [
+            isBeforeValuesAvailable ? ((props.simulationResult.barnetilleggSaerkullsbarn?.yearly.before ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.before ?? 0)) : undefined,
+            (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.after ?? 0) + (props.simulationResult.barnetilleggFellesbarn?.yearly.after ?? 0)].filter(isNotUndefined),
+          color: 'var(--a-purple-400)'
+        } : undefined,
+        {
+          ...COLUMN_STYLE(isBeforeValuesAvailable),
+          name: 'Din forventede inntekt',
+          data: [
+            isBeforeValuesAvailable ? (props.simulationResult.forventetInntekt.yearly.before ?? 0) : undefined,
+            props.simulationResult.forventetInntekt.yearly.after ?? 0].filter(isNotUndefined),
+          color: 'var(--a-green-400)'
+        }
+        ].filter(isNotUndefined)
+      }} />
+    </VStack>
+  )
 }
 
 const isNotUndefined = <T,>(value: T | undefined): value is T => value !== undefined && value !== null && value !== 0;
