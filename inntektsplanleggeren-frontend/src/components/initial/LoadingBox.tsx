e011d441759665d81e4269c0e73e022c94743ac8
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-02-26 16:54:46 +0100

    Sentrer loading ved oppstart

--- inntektsplanleggeren-frontend/src/components/initial/LoadingBox.tsx
+++ inntektsplanleggeren-frontend/src/components/initial/LoadingBox.tsx
@@ -1,15 +1,13 @@
-import {BodyShort, Box, Loader, VStack} from "@navikt/ds-react";
+import { BodyShort, Box, Loader, VStack } from "@navikt/ds-react";
 import React from "react";
 
 export function LoadingBox() {
-    return (
-        <Box background="bg-subtle" padding="16" borderRadius="large">
-            <VStack align="center" gap="20">
-                <Loader size="3xlarge"/>
-                <VStack>
-                    <BodyShort>Vent mens vi laster inn siden.</BodyShort>
-                </VStack>
-            </VStack>
-        </Box>
-    );
+  return (
+    <Box background="bg-subtle" padding="16" borderRadius="large">
+      <VStack align="center" gap="20">
+        <Loader size="3xlarge" />
+        <BodyShort align="center">Vent mens vi laster inn siden.</BodyShort>
+      </VStack>
+    </Box>
+  );
 }
