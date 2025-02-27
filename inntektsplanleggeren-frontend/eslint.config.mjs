f86ffa554de4748affbef97cf1bf6a75e06b18fb
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-02-27 15:46:37 +0100

    Use eslint for server

--- inntektsplanleggeren-frontend/eslint.config.mjs
+++ inntektsplanleggeren-frontend/eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   {
@@ -15,4 +16,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat['jsx-runtime'],
+  eslintConfigPrettier,
 ]
