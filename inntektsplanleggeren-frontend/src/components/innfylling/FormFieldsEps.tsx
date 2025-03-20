15ffabf35061546f6e584a1396aae89eed598bc8
Juliana Coelho Maccarini Skrøvseth <juliana.coelho.maccarini.skrovseth@nav.no>
2025-03-20 16:14:42 +0100

    PEB-1081 fikse heading i skjema

--- inntektsplanleggeren-frontend/src/components/innfylling/FormFieldsEps.tsx
+++ inntektsplanleggeren-frontend/src/components/innfylling/FormFieldsEps.tsx
@@ -182,7 +182,9 @@ export const FormFieldsEps = ({
 
       <Box padding="4" background="surface-subtle" borderRadius="large">
         <VStack gap={{ xs: '2', sm: '1' }}>
-          <Heading size="small"> Annen forelder sin samlede inntekt i {year}: </Heading>
+          <Heading level="4" size="small">
+            Annen forelder sin samlede inntekt i {year}:
+          </Heading>
           <BodyShort className="sum">
             <FormatKroner value={inntektSum} /> før skatt
           </BodyShort>
