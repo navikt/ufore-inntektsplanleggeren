a13f9886a9a3dcd1e5250974d58bf3b616080fd1
Juliana Coelho Maccarini Skrøvseth <juliana.coelho.maccarini.skrovseth@nav.no>
2025-04-09 16:08:04 +0200

    PEB-1128 lage en større avstand over knappen i den blåe boksen

--- inntektsplanleggeren-frontend/src/components/initial/DinInntektsgrenseCard.tsx
+++ inntektsplanleggeren-frontend/src/components/initial/DinInntektsgrenseCard.tsx
@@ -21,100 +21,102 @@ export function InntektsgrenseCard(props: { displayData: InitiateData }) {
   return (
     <Box borderRadius="xlarge" padding="4" className="top-box" ref={inntektsgrenseCardRef}>
       <VStack gap="2">
-        <Heading level="2" size="small">
-          Inntektsgrenser og trekkprosent
-        </Heading>
-        <VStack gap="5">
-          <BodyLong style={{ whiteSpace: 'pre-wrap' }}>
-            Dine inntektsgrenser sier hvor mye inntekt du kan ha før vi trekker en prosent (kompensasjonsgrad) av
-            utbetalingen din.
-          </BodyLong>
+        <VStack gap="2" style={{ marginBottom: '20px' }}>
+          <Heading level="2" size="small">
+            Inntektsgrenser og trekkprosent
+          </Heading>
+          <VStack gap="5">
+            <BodyLong style={{ whiteSpace: 'pre-wrap' }}>
+              Dine inntektsgrenser sier hvor mye inntekt du kan ha før vi trekker en prosent (kompensasjonsgrad) av
+              utbetalingen din.
+            </BodyLong>
 
-          {isOpen ? (
-            <VStack gap="5">
-              <section>
-                <Heading size="xsmall" level="3">
-                  Din inntektsgrense: <FormatKroner value={props.displayData.inntektsgrense} />
-                </Heading>
-                {props.displayData.hasVarigTilrettelagtArbeid ? (
-                  <BodyLong style={{ wordBreak: 'normal' }}>
-                    Du har tiltaket{' '}
-                    <Link variant="neutral" href={'https://www.nav.no/varig-tilrettelagt-arbeid'}>
-                      Varig tilrettelagt arbeid
-                    </Link>
-                    . Bonuslønnen din kan være inntil{' '}
-                    <FormatKroner value={props.displayData.inntektsgrense}></FormatKroner> (som tilsvarer 1 G). Tjener
-                    du mer enn dette, vil du få lavere utbetaling av uføretrygd. Vi reduserer uføretrygden din av
-                    beløpet du tjener over inntektsgrensen. Beløpet opp til inntektsgrensen blir du aldri trukket for. I
-                    de fleste tilfeller vil det lønne seg å jobbe, fordi uføretrygd og inntekt er høyere enn uføretrygd
-                    alene.
-                  </BodyLong>
-                ) : (
-                  <BodyLong style={{ wordBreak: 'normal' }}>
-                    Tjener du mer enn dette, vil du få lavere utbetaling av uføretrygd. Vi reduserer uføretrygden din av
-                    beløpet du tjener over inntektsgrensen. Beløpet opp til inntektsgrensen blir du aldri trukket for.
-                  </BodyLong>
-                )}
-              </section>
-
-              <section>
-                <Heading size="xsmall" level="3">
-                  Din trekkprosent (kompensasjonsgrad):{' '}
-                  <FormatDecimalNumber value={props.displayData.kompensasjonsgrad} /> prosent
-                </Heading>
-                <BodyLong style={{ wordBreak: 'normal' }}>
-                  Tjener du mer enn inntektsgrensen, får du lavere utbetaling av uføretrygd, ut fra din trekkprosent. Vi
-                  trekker <FormatDecimalNumber value={props.displayData.kompensasjonsgrad} /> prosent kun av det du har
-                  tjent over inntektsgrensen. Du vil fortsatt få utbetalt redusert uføretrygd i tillegg til lønnen din.
-                </BodyLong>
-              </section>
-
-              <section>
-                <Heading size="xsmall" level="3">
-                  Tjener du mer enn <FormatKroner value={props.displayData.grenseStoppAvUfoeretrygd} /> får du ikke
-                  utbetalt uføretrygd
-                </Heading>
-                <List>
-                  <List.Item>
-                    Har du inntekt på mer enn <FormatKroner value={props.displayData.grenseStoppAvUfoeretrygd} /> per
-                    år, får du ikke utbetaling av uføretrygd det aktuelle året.
-                  </List.Item>
-                  <List.Item>
-                    Tjener du mer enn <FormatKroner value={props.displayData.grenseStoppAvUfoeretrygd} /> et kalenderår,
-                    må du betale tilbake det du har fått i uføretrygd det året.
-                  </List.Item>
-                  <List.Item>
-                    Du beholder likevel retten til uføretrygd. Tjener du mindre neste år, kan du igjen få utbetalt
-                    uføretrygd.{' '}
-                  </List.Item>
-                </List>
-              </section>
-
-              {props.displayData.hasGjenlevendeTillegg ? (
+            {isOpen ? (
+              <VStack gap="5">
                 <section>
                   <Heading size="xsmall" level="3">
-                    Gjenlevendetillegg
+                    Din inntektsgrense: <FormatKroner value={props.displayData.inntektsgrense} />
                   </Heading>
-                  <BodyLong style={{ wordBreak: 'normal' }}>
-                    Tjener du mer enn inntektsgrensen din, reduseres også utbetalingen av gjenlevendetillegget ditt.
-                  </BodyLong>
+                  {props.displayData.hasVarigTilrettelagtArbeid ? (
+                    <BodyLong style={{ wordBreak: 'normal' }}>
+                      Du har tiltaket{' '}
+                      <Link variant="neutral" href={'https://www.nav.no/varig-tilrettelagt-arbeid'}>
+                        Varig tilrettelagt arbeid
+                      </Link>
+                      . Bonuslønnen din kan være inntil{' '}
+                      <FormatKroner value={props.displayData.inntektsgrense}></FormatKroner> (som tilsvarer 1 G). Tjener
+                      du mer enn dette, vil du få lavere utbetaling av uføretrygd. Vi reduserer uføretrygden din av
+                      beløpet du tjener over inntektsgrensen. Beløpet opp til inntektsgrensen blir du aldri trukket for. I
+                      de fleste tilfeller vil det lønne seg å jobbe, fordi uføretrygd og inntekt er høyere enn uføretrygd
+                      alene.
+                    </BodyLong>
+                  ) : (
+                    <BodyLong style={{ wordBreak: 'normal' }}>
+                      Tjener du mer enn dette, vil du få lavere utbetaling av uføretrygd. Vi reduserer uføretrygden din av
+                      beløpet du tjener over inntektsgrensen. Beløpet opp til inntektsgrensen blir du aldri trukket for.
+                    </BodyLong>
+                  )}
                 </section>
-              ) : null}
 
-              {props.displayData.hasBarneTilleggFellesbarn || props.displayData.hasBarnetilleggSaerkullsbarn ? (
                 <section>
                   <Heading size="xsmall" level="3">
-                    Barnetillegg
+                    Din trekkprosent (kompensasjonsgrad):{' '}
+                    <FormatDecimalNumber value={props.displayData.kompensasjonsgrad} /> prosent
                   </Heading>
                   <BodyLong style={{ wordBreak: 'normal' }}>
-                    Inntekten din har betydning for hvor mye du får utbetalt i barnetillegg. For barn som bor med begge sine foreldre, bruker vi begge foreldrenes inntekt når vi beregner størrelsen på barnetillegget.
-                    <Link variant="neutral" href={'https://www.nav.no/uforetrygd#tillegg'}>Les mer om barnetillegg til uføretrygden.</Link>
+                    Tjener du mer enn inntektsgrensen, får du lavere utbetaling av uføretrygd, ut fra din trekkprosent. Vi
+                    trekker <FormatDecimalNumber value={props.displayData.kompensasjonsgrad} /> prosent kun av det du har
+                    tjent over inntektsgrensen. Du vil fortsatt få utbetalt redusert uføretrygd i tillegg til lønnen din.
                   </BodyLong>
                 </section>
-              ) : null}
 
-            </VStack>
-          ) : null}
+                <section>
+                  <Heading size="xsmall" level="3">
+                    Tjener du mer enn <FormatKroner value={props.displayData.grenseStoppAvUfoeretrygd} /> får du ikke
+                    utbetalt uføretrygd
+                  </Heading>
+                  <List>
+                    <List.Item>
+                      Har du inntekt på mer enn <FormatKroner value={props.displayData.grenseStoppAvUfoeretrygd} /> per
+                      år, får du ikke utbetaling av uføretrygd det aktuelle året.
+                    </List.Item>
+                    <List.Item>
+                      Tjener du mer enn <FormatKroner value={props.displayData.grenseStoppAvUfoeretrygd} /> et kalenderår,
+                      må du betale tilbake det du har fått i uføretrygd det året.
+                    </List.Item>
+                    <List.Item>
+                      Du beholder likevel retten til uføretrygd. Tjener du mindre neste år, kan du igjen få utbetalt
+                      uføretrygd.{' '}
+                    </List.Item>
+                  </List>
+                </section>
+
+                {props.displayData.hasGjenlevendeTillegg ? (
+                  <section>
+                    <Heading size="xsmall" level="3">
+                      Gjenlevendetillegg
+                    </Heading>
+                    <BodyLong style={{ wordBreak: 'normal' }}>
+                      Tjener du mer enn inntektsgrensen din, reduseres også utbetalingen av gjenlevendetillegget ditt.
+                    </BodyLong>
+                  </section>
+                ) : null}
+
+                {props.displayData.hasBarneTilleggFellesbarn || props.displayData.hasBarnetilleggSaerkullsbarn ? (
+                  <section>
+                    <Heading size="xsmall" level="3">
+                      Barnetillegg
+                    </Heading>
+                    <BodyLong style={{ wordBreak: 'normal' }}>
+                      Inntekten din har betydning for hvor mye du får utbetalt i barnetillegg. For barn som bor med begge sine foreldre, bruker vi begge foreldrenes inntekt når vi beregner størrelsen på barnetillegget.
+                      <Link variant="neutral" href={'https://www.nav.no/uforetrygd#tillegg'}>Les mer om barnetillegg til uføretrygden.</Link>
+                    </BodyLong>
+                  </section>
+                ) : null}
+
+              </VStack>
+            ) : null}
+          </VStack>
         </VStack>
         <HStack justify="center">
           <Button
