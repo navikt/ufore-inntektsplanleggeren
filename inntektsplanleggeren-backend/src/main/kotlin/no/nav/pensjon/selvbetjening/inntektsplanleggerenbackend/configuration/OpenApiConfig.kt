4298bec91534addf9bbe244e5783e2829bbc0a19
Eirik Stadheim <eirik.stadheim@nav.no>
2025-09-16 10:52:47 +0200

    Autentisering i swagger

---
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/configuration/OpenApiConfig.kt
@@ -0,0 +1,28 @@
+package no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.configuration
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI =
+        OpenAPI()
+            .info(Info().title("Uføretrygd Inntektsplanlegger").version("v1"))
+            .addSecurityItem(SecurityRequirement().addList("bearerAuth"))
+            .components(
+                io.swagger.v3.oas.models.Components()
+                    .addSecuritySchemes(
+                        "bearerAuth",
+                        SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            )
+}
