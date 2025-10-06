138bdc0a90bf8c4b1c82cfc8380d287142103fb4
eirikstadheim <eirik.stadheim@nav.no>
2025-10-06 16:35:35 +0200

    Legger tilbake at vi bruker azure obo mot inntektskomponenten

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntekt/InntektskomponentClient.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/inntekt/InntektskomponentClient.kt
@@ -10,7 +10,6 @@ import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntekt.dto.Hent
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntektsplanlegger.ClientException
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntektsplanlegger.ForbiddenException
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.inntektsplanlegger.PersonNotFoundException
-import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.security.AzureAdService
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.security.TokenService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -26,8 +25,7 @@ class InntektskomponentClient(
     @Value("\${inntektskomponenten.endpoint.url}") private val url: String,
     @Value("\${inntektskomponenten.scope}") private val scope: String,
     private val webClient: WebClient,
-    private val tokenService: TokenService,
-    private val azureAdService: AzureAdService
+    private val tokenService: TokenService
 ) {
     private val logger: Logger = LoggerFactory.getLogger(InntektskomponentClient::class.java)
 
@@ -37,7 +35,7 @@ class InntektskomponentClient(
     ): HentForventetInntektResponse {
         val path = "/rs/api/v1/forventetinntekt"
         try {
-            return azureAdService.retrieveClientCredentialsToken(listOf(scope)) //TODO: Temp fix original code: tokenService.getEgressToken(scope = scope, "", AppId.INNTEKTSKOMPONENTEN)
+            return tokenService.getEgressToken(scope = scope, pid = pid, appId = AppId.INNTEKTSKOMPONENTEN)
                 .let { accessToken ->
                     webClient
                         .get()
@@ -69,7 +67,8 @@ class InntektskomponentClient(
     fun hentAbonnerteInntekterBolk(
         abonnerteInntekterIdentOgPeriodeListe: List<AbonnerteInntekterIdentOgPeriode>,
         ainntektsfilter: String,
-        formaal: String
+        formaal: String,
+        pid: String
     ): HentAbonnerteInntekterBolkResponse {
         val path = "/rs/api/v1/hentabonnerteinntekterbolk"
         val request = HentAbonnerteInntekterBolkRequest(
@@ -79,7 +78,7 @@ class InntektskomponentClient(
             abonnerteInntekterIdentOgPeriodeListe
         )
         try {
-            return azureAdService.retrieveClientCredentialsToken(listOf(scope)) //TODO: Temp fix original code: tokenService.getEgressToken(scope = scope, "", AppId.INNTEKTSKOMPONENTEN)
+            return tokenService.getEgressToken(scope = scope, pid = pid, appId = AppId.INNTEKTSKOMPONENTEN)
                 .let { accessToken ->
                     webClient
                         .post()
