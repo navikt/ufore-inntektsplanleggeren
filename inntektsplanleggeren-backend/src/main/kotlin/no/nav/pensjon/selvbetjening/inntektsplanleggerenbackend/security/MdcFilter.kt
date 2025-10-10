46982b900eade318e10217e16bbb6d5c215fa78f
eirikstadheim <eirik.stadheim@nav.no>
2025-10-10 21:30:15 +0200

    Legger saksbehandler-ident i mdc

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/MdcFilter.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/MdcFilter.kt
@@ -5,12 +5,14 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.configuration.NAV_CALL_ID
 import org.slf4j.MDC
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.util.UUID
 
 @Component
-class SetCallIdFilter: OncePerRequestFilter() {
+class MdcFilter: OncePerRequestFilter() {
 
     override fun doFilterInternal(
         request: HttpServletRequest,
@@ -18,6 +20,12 @@ class SetCallIdFilter: OncePerRequestFilter() {
         filterChain: FilterChain
     ) {
         MDC.put(NAV_CALL_ID, request.getHeader(NAV_CALL_ID) ?: UUID.randomUUID().toString())
+
+        (SecurityContextHolder.getContext().authentication as JwtAuthenticationToken).token.getClaim<String>("NAVident")
+            ?.let { saksbehandler ->
+                MDC.put("saksbehandler", saksbehandler)
+            }
+
         filterChain.doFilter(request, response)
     }
 }
