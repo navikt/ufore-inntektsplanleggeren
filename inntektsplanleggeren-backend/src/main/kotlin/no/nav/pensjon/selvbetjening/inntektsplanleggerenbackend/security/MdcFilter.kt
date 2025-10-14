d46e664ec0faede24008af4b1f12aff70071084e
eirikstadheim <eirik.stadheim@nav.no>
2025-10-14 10:42:21 +0200

    Legger maskert pid på mdc

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/MdcFilter.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/MdcFilter.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.configuration.NAV_CALL_ID
+import no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.util.Masker
 import org.slf4j.MDC
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
@@ -12,7 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter
 import java.util.UUID
 
 @Component
-class MdcFilter: OncePerRequestFilter() {
+class MdcFilter : OncePerRequestFilter() {
 
     override fun doFilterInternal(
         request: HttpServletRequest,
@@ -22,10 +23,16 @@ class MdcFilter: OncePerRequestFilter() {
         MDC.put(NAV_CALL_ID, request.getHeader(NAV_CALL_ID) ?: UUID.randomUUID().toString())
 
         if (SecurityContextHolder.getContext().authentication is JwtAuthenticationToken) {
-            (SecurityContextHolder.getContext().authentication as JwtAuthenticationToken).token.getClaim<String>("NAVident")
-                ?.let { saksbehandler ->
-                    MDC.put("saksbehandler", saksbehandler)
-                }
+            (SecurityContextHolder.getContext().authentication as JwtAuthenticationToken).token.let { token ->
+                token.getClaim<String>("NAVident")
+                    ?.let { saksbehandler ->
+                        MDC.put("nav_ident", saksbehandler)
+                    }
+                token.getClaim<String>("pid")
+                    ?.let { pid ->
+                        MDC.put("pid", Masker.maskPid(pid))
+                    }
+            }
         }
 
         filterChain.doFilter(request, response)
