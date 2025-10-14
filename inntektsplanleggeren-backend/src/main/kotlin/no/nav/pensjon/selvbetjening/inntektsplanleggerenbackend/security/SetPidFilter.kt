bee7aa00afa9e93e5c77f214b0018a61f0b2179c
Anders Bye <anders.bye@nav.no>
2025-10-14 12:49:46 +0200

    PEB-1316 reduser stoy i logg

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/SetPidFilter.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/SetPidFilter.kt
@@ -64,7 +64,7 @@ class SetPidFilter(
                     logger.info("Pid is encrypted. Decrypting...")
                     pidEncryptionClient.decrypt(pidFromHeader)!!
                 } else {
-                    logger.warn("Using unencrypted PID from request")
+                    logger.info("Using unencrypted PID from request :-(")
                     pidFromHeader
                 }
                 log.info("Veileder on behalf of ${Masker.maskPid(pid)}")
