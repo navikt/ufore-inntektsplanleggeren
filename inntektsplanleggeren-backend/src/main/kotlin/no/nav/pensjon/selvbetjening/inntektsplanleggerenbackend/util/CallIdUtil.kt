097e977935e83a445e28b921c25d9693be19b944
eirikstadheim <eirik.stadheim@nav.no>
2025-10-15 12:26:51 +0200

    Endrer mdcfilter til å bruke tokenService og legger på obo_pid dersom fullmakt

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/util/CallIdUtil.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/util/CallIdUtil.kt
@@ -2,6 +2,4 @@ package no.nav.pensjon.selvbetjening.inntektsplanleggerenbackend.util
 
 import org.slf4j.MDC
 
-const val NAV_CALL_ID = "Nav-Call-Id"
-const val NAV_CALL_ID_MDC = "nav_call_id"
-fun getCurrentCallId(): String? = MDC.get(NAV_CALL_ID)
+fun getCurrentCallId(): String? = MDC.get(NAV_CALL_ID_MDC)
