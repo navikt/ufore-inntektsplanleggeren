19bf22a4870283419efe61c910b6d1ca4cd2b90c
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2025-11-04 14:07:45 +0100

    wip

--- inntektsplanleggeren-frontend/src/components/beregning/BeregningWarnings.tsx
+++ inntektsplanleggeren-frontend/src/components/beregning/BeregningWarnings.tsx
@@ -28,7 +28,8 @@ export function BeregningWarnings(props: { messages: Message[] }) {
         (message) => message.messageCode === MessageCodes.OPPGITT_INNTEKT_GIVES_MORE_UFORETRYGD_THAN_ALREADY_UTBETALT
       ) && (
         <Alert variant="info">
-          Inntekten du har lagt inn, er lavere enn inntekten vi har brukt til å beregne uføretrygden din. Derfor får du ikke trekk i uføretrygden din resten av året.
+          Inntekten du har lagt inn, er lavere enn inntekten vi har brukt til å beregne uføretrygden din.
+            Derfor får du ikke trekk i uføretrygden din resten av året.
           Har du fått utbetalt for lite uføretrygd ved slutten av året, kan du få tilbake penger i etteroppgjøret.
         </Alert>
       )}
