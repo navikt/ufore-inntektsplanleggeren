b4a25ca1f132e2ffb2da67e3be1a1708d1682541
eirikstadheim <eirik.stadheim@nav.no>
2025-11-26 11:52:21 +0100

    Setter order på filtrene, så vi er sikker på at mdc er initialisert

--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/SetPidFilter.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/pensjon/selvbetjening/inntektsplanleggerenbackend/security/SetPidFilter.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
@@ -13,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDateTime
 
 @Component
+@Order(2)
 class SetPidFilter(
     private val tokenService: TokenService,
     private val authorizationService: AuthorizationService,
