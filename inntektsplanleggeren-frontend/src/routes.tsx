883d1c62b256c12902e458311c25a087c80c50c9
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2025-12-02 09:57:14 +0100

    Legger til side for å vise at applikasjonen er nede for vedlikehold

--- inntektsplanleggeren-frontend/src/routes.tsx
+++ inntektsplanleggeren-frontend/src/routes.tsx
@@ -1,47 +1,46 @@
 import { useContext } from 'react'
-import { Routes, Route, BrowserRouter, Outlet, Navigate } from 'react-router-dom'
-import { InitialPage } from '@/components/initial/InitialPage'
-import App from '@/App'
-import { FormContainer, PageLinks } from '@/FormContainer'
-import { InnfyllingPage } from '@/components/innfylling/InnfyllingPage'
-import { BeregningPage } from '@/components/beregning/BeregningPage'
-import { KvitteringPage } from '@/components/kvittering/KvitteringPage'
+import { Navigate, Outlet } from 'react-router-dom'
 import { FormStateContext } from '@/context/FormData'
 import { SelectedYearProvider } from '@/context/SelectedYear'
-import { OppsummeringPage } from '@/components/oppsummering/OppsummeringPage'
-import { PreviousYearPage } from '@/components/previousYear/PreviousYearPage'
+import { NedetidPage } from '@/components/nedetid/NedetidPage'
 
 export const BASE_PATH = '/uforetrygd/selvbetjening/inntektsplanleggeren'
 
-export const AppRoutes = () => (
-  <BrowserRouter basename={BASE_PATH}>
-    <Routes>
-      <Route element={<App />}>
-        <Route index element={<InitialPage />} />
-        <Route index path={PageLinks.FORRIGE_INNTEKTER} element={<PreviousYearPage />} />
-        <Route element={<YearGuard />}>
-          <Route element={<FormContainer />}>
-            <Route index path={PageLinks.FORVENTET_INNTEKT} element={<InnfyllingPage />} />
-            <Route index path={PageLinks.BEREGNING} element={<BeregningPage />} />
-            <Route index path={PageLinks.OPPSUMMERING} element={<OppsummeringPage />} />
-            <Route index path={PageLinks.KVITTERING} element={<KvitteringPage />} />
-          </Route>
-        </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-)
+export const AppRoutes = () => {
+
+    // TODO: få på plass feature toggling
+    return <NedetidPage />
+
+    // return (
+    //     <BrowserRouter basename={BASE_PATH}>
+    //         <Routes>
+    //             <Route element={<App />}>
+    //                 <Route index element={<InitialPage />} />
+    //                 <Route index path={PageLinks.FORRIGE_INNTEKTER} element={<PreviousYearPage />} />
+    //                 <Route element={<YearGuard />}>
+    //                     <Route element={<FormContainer />}>
+    //                         <Route index path={PageLinks.FORVENTET_INNTEKT} element={<InnfyllingPage />} />
+    //                         <Route index path={PageLinks.BEREGNING} element={<BeregningPage />} />
+    //                         <Route index path={PageLinks.OPPSUMMERING} element={<OppsummeringPage />} />
+    //                         <Route index path={PageLinks.KVITTERING} element={<KvitteringPage />} />
+    //                     </Route>
+    //                 </Route>
+    //             </Route>
+    //         </Routes>
+    //     </BrowserRouter>
+    // )
+}
 
 const YearGuard = () => {
-  const { selectedYear } = useContext(FormStateContext)
+    const { selectedYear } = useContext(FormStateContext)
 
-  if (selectedYear === null || selectedYear === 0) {
-    return <Navigate to="/" replace />
-  }
+    if (selectedYear === null || selectedYear === 0) {
+        return <Navigate to="/" replace />
+    }
 
-  return (
-    <SelectedYearProvider selectedYear={selectedYear}>
-      <Outlet />
-    </SelectedYearProvider>
-  )
+    return (
+        <SelectedYearProvider selectedYear={selectedYear}>
+            <Outlet />
+        </SelectedYearProvider>
+    )
 }
