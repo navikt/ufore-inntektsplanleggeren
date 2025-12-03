a41f03028f0683c85c737040b115c6d4b0f0f33a
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2025-12-03 08:40:56 +0100

    Åpner Inntektsplanleggeren igjen etter planlagt stenging

--- inntektsplanleggeren-frontend/src/routes.tsx
+++ inntektsplanleggeren-frontend/src/routes.tsx
@@ -1,34 +1,41 @@
 import { useContext } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 import { FormStateContext } from '@/context/FormData'
 import { SelectedYearProvider } from '@/context/SelectedYear'
-import { NedetidPage } from '@/components/nedetid/NedetidPage'
+import { BrowserRouter } from 'react-router'
+import App from '@/App'
+import { InitialPage } from '@/components/initial/InitialPage'
+import { FormContainer, PageLinks } from '@/FormContainer'
+import { PreviousYearPage } from '@/components/previousYear/PreviousYearPage'
+import { InnfyllingPage } from '@/components/innfylling/InnfyllingPage'
+import { BeregningPage } from '@/components/beregning/BeregningPage'
+import { OppsummeringPage } from '@/components/oppsummering/OppsummeringPage'
+import { KvitteringPage } from '@/components/kvittering/KvitteringPage'
 
 export const BASE_PATH = '/uforetrygd/selvbetjening/inntektsplanleggeren'
 
 export const AppRoutes = () => {
-
     // TODO: få på plass feature toggling
-    return <NedetidPage />
+    //return <NedetidPage />
 
-    // return (
-    //     <BrowserRouter basename={BASE_PATH}>
-    //         <Routes>
-    //             <Route element={<App />}>
-    //                 <Route index element={<InitialPage />} />
-    //                 <Route index path={PageLinks.FORRIGE_INNTEKTER} element={<PreviousYearPage />} />
-    //                 <Route element={<YearGuard />}>
-    //                     <Route element={<FormContainer />}>
-    //                         <Route index path={PageLinks.FORVENTET_INNTEKT} element={<InnfyllingPage />} />
-    //                         <Route index path={PageLinks.BEREGNING} element={<BeregningPage />} />
-    //                         <Route index path={PageLinks.OPPSUMMERING} element={<OppsummeringPage />} />
-    //                         <Route index path={PageLinks.KVITTERING} element={<KvitteringPage />} />
-    //                     </Route>
-    //                 </Route>
-    //             </Route>
-    //         </Routes>
-    //     </BrowserRouter>
-    // )
+    return (
+        <BrowserRouter basename={BASE_PATH}>
+            <Routes>
+                <Route element={<App />}>
+                    <Route index element={<InitialPage />} />
+                    <Route index path={PageLinks.FORRIGE_INNTEKTER} element={<PreviousYearPage />} />
+                    <Route element={<YearGuard />}>
+                        <Route element={<FormContainer />}>
+                            <Route index path={PageLinks.FORVENTET_INNTEKT} element={<InnfyllingPage />} />
+                            <Route index path={PageLinks.BEREGNING} element={<BeregningPage />} />
+                            <Route index path={PageLinks.OPPSUMMERING} element={<OppsummeringPage />} />
+                            <Route index path={PageLinks.KVITTERING} element={<KvitteringPage />} />
+                        </Route>
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 const YearGuard = () => {
