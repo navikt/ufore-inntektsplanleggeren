2177fb8d7dfb9effa3f011528e8c923b05b82e14
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2026-05-19 11:14:42 +0200

    Prøver å få steng-for-g-regulering til å funke - gjør det likt som alder

--- inntektsplanleggeren-frontend/server/server.ts
+++ inntektsplanleggeren-frontend/server/server.ts
@@ -131,7 +131,7 @@ app.get('/internal/health/readiness', (req, res) => {
         status: 'UP',
     })
 })
-app.use(stengForReguleringMiddleware({ env: isDevelopment ? 'dev' : 'prod', unleashClient: unleash }))
+app.use(stengForReguleringMiddleware())
 app.use(metricsMiddleware)
 app.use(correlationIdMiddleware)
 app.use(loggerMiddleware(logger))
