5f6d96e24085bb91c7e3af26803f06514501f899
Sindre Rasmussen <sindre.rasmussen@nav.no>
2026-06-09 11:51:23 +0200

    PEB-1603: No space in comma separated list.

--- inntektsplanleggeren-frontend/src/components/common/Banner/BorgerBanner.tsx
+++ inntektsplanleggeren-frontend/src/components/common/Banner/BorgerBanner.tsx
@@ -3,7 +3,7 @@ export default function BorgerBanner  ()  {
     return (
         <div >
             <representasjon-banner
-                representasjonstyper="UFORETRYGD_LES, UFORETRYGD_SKRIV, VERGE_UFORETRYGD_LES, VERGE_UFORETRYGD_SKRIV"
+                representasjonstyper="UFORETRYGD_LES,UFORETRYGD_SKRIV,VERGE_UFORETRYGD_LES,VERGE_UFORETRYGD_SKRIV"
                 redirectTo={`${window.location.origin}/uforetrygd/selvbetjening`}
             ></representasjon-banner>
         </div>
