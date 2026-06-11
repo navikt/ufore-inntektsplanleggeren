5025d85c4776b84762808b4c1d2f52ce47b402db
Sindre Rasmussen <sindre.rasmussen@nav.no>
2026-06-11 13:52:20 +0200

    PEB-1603: Revert using origin location in breadcrumbs.

--- inntektsplanleggeren-frontend/src/components/common/Banner/BorgerBanner.tsx
+++ inntektsplanleggeren-frontend/src/components/common/Banner/BorgerBanner.tsx
@@ -6,9 +6,9 @@ export default function BorgerBanner  ()  {
                 representasjonstyper="UFORETRYGD_LES,UFORETRYGD_SKRIV,VERGE_UFORETRYGD_LES,VERGE_UFORETRYGD_SKRIV"
                 redirectTo={`${window.location.origin}/uforetrygd/selvbetjening/inntektsplanleggeren`}
                 breadcrumbs={JSON.stringify([
-                    { url: `${window.location.origin}/minside`, title: "Min side" },
-                    { url: `${window.location.origin}/uforetrygd/selvbetjening`, title: "Din uføretrygd" },
-                    { url: `${window.location.origin}/uforetrygd/selvbetjening/inntektsplanleggeren`, title: "Inntektsplanleggeren" },
+                    { url: "https://www.nav.no/minside", title: "Min side" },
+                    { url: "https://www.nav.no/uforetrygd/selvbetjening", title: "Din uføretrygd" },
+                    { url: "https://www.nav.no/uforetrygd/selvbetjening/inntektsplanleggeren", title: "Inntektsplanleggeren" },
                 ])}
             ></representasjon-banner>
         </div>
