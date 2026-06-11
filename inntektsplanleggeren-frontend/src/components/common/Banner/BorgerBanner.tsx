3b90ed254b441aff3c775cec6c32c99522136006
Sindre Rasmussen <sindre.rasmussen@nav.no>
2026-06-11 13:12:26 +0200

    PEB-1603: Added breadcrumbs in representasjon banner.

--- inntektsplanleggeren-frontend/src/components/common/Banner/BorgerBanner.tsx
+++ inntektsplanleggeren-frontend/src/components/common/Banner/BorgerBanner.tsx
@@ -5,6 +5,11 @@ export default function BorgerBanner  ()  {
             <representasjon-banner
                 representasjonstyper="UFORETRYGD_LES,UFORETRYGD_SKRIV,VERGE_UFORETRYGD_LES,VERGE_UFORETRYGD_SKRIV"
                 redirectTo={`${window.location.origin}/uforetrygd/selvbetjening`}
+                breadcrumbs={JSON.stringify([
+                    { url: "https://www.nav.no/minside", title: "Min side" },
+                    { url: "https://www.nav.no/uforetrygd/selvbetjening", title: "Din uføretrygd" },
+                    { url: "https://www.nav.no/uforetrygd/selvbetjening/inntektsplanleggeren", title: "Inntektsplanleggeren" },
+                ])}
             ></representasjon-banner>
         </div>
     )
