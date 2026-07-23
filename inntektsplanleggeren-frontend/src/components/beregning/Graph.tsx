1cff090563d6cdafc718a927d3667afb9b90c295
Hanne Æsøy Nes <48721817+hanneanes@users.noreply.github.com>
2026-07-23 10:30:36 +0200

    Bytter til den nye offisielle highcharts (#609)

--- inntektsplanleggeren-frontend/src/components/beregning/Graph.tsx
+++ inntektsplanleggeren-frontend/src/components/beregning/Graph.tsx
@@ -1,6 +1,8 @@
+import { Chart, XAxis, YAxis } from '@highcharts/react'
+import { ColumnSeries } from '@highcharts/react/series/Column'
 import { VStack } from '@navikt/ds-react'
-import Highcharts, { type Options } from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
+import type Highcharts from 'highcharts'
+import type { Options } from 'highcharts'
 import { useEffect, useState } from 'react'
 import type { SimulationResult } from '@/api/model/ApiRequests'
 import { DESKTOP_WIDTH } from '@/FormContainer'
@@ -23,94 +25,8 @@ const getXAxisCategories = (isBeforeValuesAvailable: boolean) => {
     }
 }
 
-const numberOfItems = (simulationResult: SimulationResult) => {
+const numberOfItems = (simulationResult: SimulationResult): number => {
     return simulationResult.barnetilleggFellesbarn || simulationResult.barnetilleggSaerkullsbarn ? 3 : 2
-}
-
-const GRAPH_DATA = (isBeforeValuesAvailable: boolean, isDesktop: boolean, numberOfItems: number) => {
-    return {
-        chart: {
-            type: 'column',
-            marginTop: 50,
-            height: 400 + numberOfItems * 37,
-        },
-        title: undefined,
-        credits: undefined,
-        xAxis: {
-            categories: getXAxisCategories(isBeforeValuesAvailable),
-            labels: {
-                style: {
-                    fontSize: isDesktop ? 18 : 16,
-                    fontWeight: 600,
-                    fontFamily: 'var(--ax-font-family)',
-                },
-            },
-        },
-        yAxis: {
-            min: 0,
-            maxPadding: 0.1,
-            title: {
-                align: 'high',
-                offset: isDesktop ? 13 : -50,
-                text: isDesktop ? 'Kroner' : 'Tusen kroner',
-                rotation: 0,
-                y: -30,
-                style: {
-                    fontSize: '16px',
-                    fontFamily: 'var(--ax-font-family)',
-                },
-            },
-            stackLabels: {
-                enabled: true,
-                formatter(this: Highcharts.StackItemObject) {
-                    return `Sum ${NUMBER_FORMATTER.format(this.total)} kr`
-                },
-                style: {
-                    fontSize: isDesktop ? 18 : 16,
-                    textOutline: false,
-                    fontWeight: 600,
-                    fontFamily: 'var(--ax-font-family)',
-                    align: 'center',
-                },
-            },
-            labels: {
-                formatter: ({ value }: { value: string | number }) => formatYAxisNumber(isDesktop, value),
-                style: {
-                    fontSize: 16,
-                    fontFamily: 'var(--ax-font-family)',
-                },
-            },
-        },
-        plotOptions: {
-            column: {
-                stacking: 'normal',
-                dataLabels: {
-                    enabled: true,
-                    padding: 10,
-                },
-            },
-        },
-        legend: {
-            events: {
-                itemClick: () => false,
-            },
-            align: 'left',
-            x: 0,
-            enableMouseTracking: false,
-            symbolHeight: 15, //size of legend circle
-            itemHoverStyle: {
-                color: '#010B18AD',
-            },
-            itemStyle: {
-                color: '#010B18AD',
-                fontSize: '18px',
-                newLine: true,
-                cursor: 'auto',
-            },
-            itemDistance: numberOfItems > 2 ? 80 : 20,
-            itemMarginBottom: 15,
-        },
-    }
 }
 
 const COLUMN_STYLE = {
@@ -145,51 +61,124 @@ export const Graph = (props: { simulationResult: SimulationResult }) => {
 
     return (
         <VStack>
-            <HighchartsReact
-                highcharts={Highcharts}
+            <Chart
+                margin={[50, 0]}
+                height={400 + numberOfItems(props.simulationResult) * 37}
+                type="column"
+                title=""
+                credits=""
                 options={{
-                    ...GRAPH_DATA(isBeforeValuesAvailable, isDesktop, numberOfItems(props.simulationResult)),
+                    palette: {
+                        colorScheme: 'light',
+                    },
+                    plotOptions: {
+                        column: {
+                            stacking: 'normal',
+                        },
+                    },
+                    legend: {
+                        events: {
+                            itemClick: () => false,
+                        },
+                        align: 'left',
+                        x: 0,
+                        symbolHeight: 15, //size of legend circle
+                        itemHoverStyle: {
+                            color: '#010B18AD',
+                        },
+                        itemStyle: {
+                            color: '#010B18AD',
+                            fontSize: '18px',
+                            newLine: true,
+                            cursor: 'auto',
+                        },
+                        itemDistance: numberOfItems(props.simulationResult) > 2 ? 80 : 20,
+                        itemMarginBottom: 15,
+                    },
+
                     tooltip,
-                    series: [
-                        {
-                            ...COLUMN_STYLE,
-                            name: props.simulationResult.gjenlevendetillegg ? 'Uføretrygd inkludert gjenlevendetillegg' : 'Uføretrygd',
-                            data: [
-                                isBeforeValuesAvailable &&
-                                    (props.simulationResult.uforetrygd.yearly.before ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.before ?? 0),
-                                (props.simulationResult.uforetrygd.yearly.after ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.after ?? 0),
-                            ].filter(isNotFalse),
-                            color: 'var(--ax-brand-blue-600)',
-                        },
-                        props.simulationResult.barnetilleggFellesbarn || props.simulationResult.barnetilleggSaerkullsbarn
-                            ? {
-                                  ...COLUMN_STYLE,
-                                  name: 'Barnetillegg uføretrygd',
-                                  data: [
-                                      isBeforeValuesAvailable &&
-                                          (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.before ?? 0) +
-                                              (props.simulationResult.barnetilleggFellesbarn?.yearly.before ?? 0),
-                                      (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.after ?? 0) +
-                                          (props.simulationResult.barnetilleggFellesbarn?.yearly.after ?? 0),
-                                  ].filter(isNotFalse),
-                                  color: '#A33069',
-                              }
-                            : undefined,
-                        {
-                            ...COLUMN_STYLE,
-                            name: 'Din forventede inntekt',
-                            data: [
-                                isBeforeValuesAvailable && (props.simulationResult.forventetInntekt.yearly.before ?? 0),
-                                props.simulationResult.forventetInntekt.yearly.after ?? 0,
-                            ].filter(isNotFalse),
-                            color: 'var(--ax-success-500)',
-                        },
-                    ].filter(isNotUndefined),
                 }}
-            />
+            >
+                <XAxis
+                    categories={getXAxisCategories(isBeforeValuesAvailable)}
+                    labels={{
+                        style: {
+                            fontSize: isDesktop ? 18 : 16,
+                            fontWeight: '600',
+                            fontFamily: 'var(--ax-font-family)',
+                        },
+                    }}
+                />
+                <YAxis
+                    min={0}
+                    maxPadding={0.1}
+                    title={{
+                        align: 'high',
+                        offset: isDesktop ? 13 : -50,
+                        text: isDesktop ? 'Kroner' : 'Tusen kroner',
+                        rotation: 0,
+                        y: -30,
+                        style: {
+                            fontSize: '16px',
+                            fontFamily: 'var(--ax-font-family)',
+                        },
+                    }}
+                    stackLabels={{
+                        enabled: true,
+                        formatter(this: Highcharts.StackItemObject) {
+                            return `Sum ${NUMBER_FORMATTER.format(this.total)} kr`
+                        },
+                        style: {
+                            fontSize: isDesktop ? 18 : 16,
+                            fontWeight: '600',
+                            fontFamily: 'var(--ax-font-family)',
+                            align: 'center',
+                        },
+                    }}
+                    labels={{
+                        formatter: ({ value }: { value: string | number }) => formatYAxisNumber(isDesktop, value),
+                        style: {
+                            fontSize: 16,
+                            fontFamily: 'var(--ax-font-family)',
+                        },
+                    }}
+                />
+                <ColumnSeries
+                    name={props.simulationResult.gjenlevendetillegg ? 'Uføretrygd inkludert gjenlevendetillegg' : 'Uføretrygd'}
+                    data={[
+                        isBeforeValuesAvailable &&
+                            (props.simulationResult.uforetrygd.yearly.before ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.before ?? 0),
+                        (props.simulationResult.uforetrygd.yearly.after ?? 0) + (props.simulationResult.gjenlevendetillegg?.yearly.after ?? 0),
+                    ].filter(isNotFalse)}
+                    color="var(--ax-brand-blue-600)"
+                    {...COLUMN_STYLE}
+                />
+                {props.simulationResult.barnetilleggFellesbarn || props.simulationResult.barnetilleggSaerkullsbarn ? (
+                    <ColumnSeries
+                        {...COLUMN_STYLE}
+                        name="Barnetillegg uføretrygd"
+                        data={[
+                            isBeforeValuesAvailable &&
+                                (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.before ?? 0) +
+                                    (props.simulationResult.barnetilleggFellesbarn?.yearly.before ?? 0),
+                            (props.simulationResult.barnetilleggSaerkullsbarn?.yearly.after ?? 0) +
+                                (props.simulationResult.barnetilleggFellesbarn?.yearly.after ?? 0),
+                        ].filter(isNotFalse)}
+                        color="#A33069"
+                    />
+                ) : null}
+                <ColumnSeries
+                    {...COLUMN_STYLE}
+                    name="Din forventede inntekt"
+                    data={[
+                        isBeforeValuesAvailable && (props.simulationResult.forventetInntekt.yearly.before ?? 0),
+                        props.simulationResult.forventetInntekt.yearly.after ?? 0,
+                    ].filter(isNotFalse)}
+                    color="var(--ax-success-500)"
+                />
+            </Chart>
         </VStack>
     )
 }
 
-const isNotUndefined = (value: unknown | undefined) => value !== undefined
-const isNotFalse = <T,>(value: T | undefined): value is T => value !== false
+const isNotFalse = <T,>(value: T | false): value is T => value !== false
