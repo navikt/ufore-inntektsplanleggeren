637ddaa3a80ca32e20c5db26672ddd2ec03b5f57
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2026-08-04 16:04:34 +0200

    Laster ikke dekoratoren i testene fordi den gjør at testene av og til feiler

--- inntektsplanleggeren-frontend/playwright.config.ts
+++ inntektsplanleggeren-frontend/playwright.config.ts
@@ -5,13 +5,12 @@ export default defineConfig({
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     workers: 3,
-    failOnFlakyTests: false, // todo, endre tilbake
+    failOnFlakyTests: true,
     retries: process.env.CI ? 2 : 0,
     reporter: [['list'], ['html', { open: 'never' }]],
     use: {
         baseURL: 'http://localhost:5173/uforetrygd/selvbetjening/inntektsplanleggeren',
         trace: 'retain-on-failure',
-        video: 'retain-on-failure',
         screenshot: 'only-on-failure',
     },
     projects: [
--- inntektsplanleggeren-frontend/tests/mock-server.spec.ts
+++ inntektsplanleggeren-frontend/tests/mock-server.spec.ts
@@ -1,13 +1,10 @@
 import { expect, test } from '@playwright/test'
 
 test('Test enkel skjemaflyt', async ({ page }) => {
-    await page.goto('/')
+    // Hindrer at dekoratøren lastes fordi den er treg, og cookie banneret av og til ikke laster
+    await page.route(/nav\.no|uxsignals\.com/, (route) => route.abort())
 
-    const cookieJaKnapp = page.getByTestId('consent-banner-all')
-    // Lukk banner hvis det finnes, uten å feile om det ikke er der
-    if (await cookieJaKnapp.isVisible()) {
-        await cookieJaKnapp.click()
-    }
+    await page.goto('/')
 
     const mainHeading = page.getByRole('heading', { name: 'Inntektsplanleggeren', level: 1 })
     await expect(mainHeading, 'Tittel på hovedside vises').toBeVisible()
