4ce313c7dd4798eaf00a2fc2eefcd814e9d9131e
ludviklund <lulu@computas.com>
2026-08-20 15:51:06 +0200

    Oppdatert henting av secrets lokalt med ny måte fra Nais

--- inntektsplanleggeren-backend/fetch-secrets.sh
+++ inntektsplanleggeren-backend/fetch-secrets.sh
@@ -1,7 +1,27 @@
-# Henter secrets for inntektsplanleggeren-backend og lagrer de under /private/tmp/ - her slettes alt ved restart av mac
-# Sett opp 2run configuration" til å "Enable EnvFile" - og pek på rett fil
-# Fungerer for mac.. (pga /private/tmp/)
-currentDir=$(pwd)
-cd /private/tmp/
-node $currentDir/../utils/fetch-secrets-to-env/dist/fetch-secrets.js ufore inntektsplanleggeren-backend-q2 azure,tokenx inntektsplanlegger.env
-cd $currentDir
+#!/usr/bin/env bash
+
+# Henter TokenX- og AzureAd-secrets for inntektsplanleggeren-backend og lagrer de under /tmp/ - her slettes alt ved restart av maskinen
+
+envFile="/tmp/inntektsplanleggeren.env"
+reason="Kjøre inntektsplanleggeren-backend lokalt"
+
+accessToken=$(printf 'n\n' | nais auth print-access-token --nais 2>/dev/null)
+
+# Logg inn hvis output ikke inneholder gyldig JWT
+if [[ ! "$accessToken" =~ ^eyJ[^.]+\.[^.]+\..+ ]]; then
+  nais login --nais -y
+fi
+
+echo "Henter AzureAd secrets..."
+nais secret get \
+    "$(nais app env inntektsplanleggeren-backend-q2 --team ufore --environment dev-gcp --output json \
+              | jq -r 'map(select(.name | startswith("AZURE_")) | .source.name) | unique[]')" \
+    --team ufore --environment dev-gcp --with-values --reason "$reason" --output json | jq -r '.data[] | "\(.key)=\(.value)"' > $envFile
+
+echo "Henter TokenX secrets..."
+nais secret get \
+    "$(nais app env inntektsplanleggeren-backend-q2 --team ufore --environment dev-gcp --output json \
+              | jq -r 'map(select(.name | startswith("TOKEN_X_")) | .source.name) | unique[]')" \
+      --team ufore --environment dev-gcp --with-values --reason "$reason" --output json | jq -r '.data[] | "\(.key)=\(.value)"' >> $envFile
+
+echo "Lagret secrets i $envFile"
--- inntektsplanleggeren-backend/src/main/kotlin/no/nav/ufore/inntektsplanleggeren/InntektsplanleggerenBackendApplication.kt
+++ inntektsplanleggeren-backend/src/main/kotlin/no/nav/ufore/inntektsplanleggeren/InntektsplanleggerenBackendApplication.kt
@@ -2,10 +2,27 @@ package no.nav.ufore.inntektsplanleggeren
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
 
 @SpringBootApplication
 class InntektsplanleggerenBackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<no.nav.ufore.inntektsplanleggeren.InntektsplanleggerenBackendApplication>(*args)
+	fetchSecretsLokalt()
+	runApplication<InntektsplanleggerenBackendApplication>(*args)
+}
+
+fun fetchSecretsLokalt() {
+	val isLocal = System.getProperty("spring.profiles.active")
+		?.split(",")
+		?.contains("local") == true
+
+	val secretsFile = File("/tmp/inntektsplanleggeren.env")
+
+	if (isLocal && !secretsFile.exists()) {
+		ProcessBuilder("./inntektsplanleggeren-backend/fetch-secrets.sh")
+			.inheritIO()
+			.start()
+			.waitFor()
+	}
 }
